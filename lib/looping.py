e22361ddff1659d2629a3d3b332e9717450b5552
kelvin <kelkipro@gmail.com>
2025-02-15 13:26:18 +0300

    loops lab

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,26 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+   number = 10             #starts from 10
+   while number > 0:
+        print(number) 
+        number -= 1
+   print("Happy New Year!")    
+          
+        
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    return [i ** 2 for i in int_list]
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
